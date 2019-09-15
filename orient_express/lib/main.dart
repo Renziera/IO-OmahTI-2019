23378e29c0f5c8d09e457f012a2c0279ef10209e
Stanley Heryanto <stanleyheryanto@Gmail.com>
2019-09-15 21:13:35 +0700

    App done

--- orient_express/lib/main.dart
+++ orient_express/lib/main.dart
@@ -1,111 +1,206 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Baker Street',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: LoginPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class LoginPage extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _LoginPageState createState() => _LoginPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _LoginPageState extends State<LoginPage> {
+  final TextEditingController _controller = TextEditingController();
 
-  void _incrementCounter() {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Masuk'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'It\'s hidden in plain sight. Use fire to expel those phantoms.',
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 16),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextField(
+                controller: _controller,
+                keyboardType: TextInputType.text,
+                maxLength: 6,
+                textAlign: TextAlign.center,
+                onSubmitted: (String s) async {
+                  _controller.clear();
+                  if (s.isEmpty) return;
+                  DocumentSnapshot ds = await Firestore.instance
+                      .collection('tim')
+                      .document(s)
+                      .get();
+                  if (ds.exists) {
+                    if (ds.data['solved'] ?? false) {
+                      await ds.reference.updateData({
+                        'solved': true,
+                        'waktu': FieldValue.serverTimestamp(),
+                      });
+                    }
+                    Navigator.of(context).pushReplacement(
+                      MaterialPageRoute(
+                        builder: (context) => ExplanationPage(
+                          docRef: ds.reference,
+                        ),
+                      ),
+                    );
+                  }
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ExplanationPage extends StatelessWidget {
+  final DocumentReference docRef;
+
+  const ExplanationPage({Key key, @required this.docRef}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Disclaimer'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.send),
+        onPressed: () {
+          Navigator.of(context).pushReplacement(
+            MaterialPageRoute(
+              builder: (context) => IdeaPage(
+                docRef: docRef,
+              ),
+            ),
+          );
+        },
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8),
+          child: Text(
+              '\nPertama-tama, saya ucapkan selamat karena kalian telah sampai di sini.\n\nAplikasi ini memang jelek, jangan tertawakan saya. Jangan lupa teman anda saya tawan. Untuk membebaskannya ada satu Quest lagi yang harus kalian selesaikan. Yaitu kalian harus membuat suatu produk MVP (Minimum Viable Product). Produk dapat berupa apa saja, mulai dari aplikasi mobile, web, maupun game dengan tema bebas. Yang akan saya lihat adalah kekompakan serta kegigihan kalian dalam membuatnya.\n\nSaya tahu kalian masih maba, jadi saya beri keringanan yaitu kalian boleh berkonsultasi dengan orang-orang ini:\n\nStanley Heryanto\nYusuf Yudhistira\n\nJika kalian sudah tahu apa yang akan dibuat, silahkan cantumkan ide kalian di halaman berikutnya.\n\nNB: Aplikasi ini tidak bisa di decompile, jangan coba-coba :v'),
+        ),
+      ),
+    );
+  }
+}
+
+class IdeaPage extends StatefulWidget {
+  final DocumentReference docRef;
+
+  const IdeaPage({Key key, @required this.docRef}) : super(key: key);
+  @override
+  _IdeaPageState createState() => _IdeaPageState();
+}
+
+class _IdeaPageState extends State<IdeaPage> {
+  final TextEditingController _timController = TextEditingController();
+  final TextEditingController _ideController = TextEditingController();
+  List<bool> selections = [false, false, false, false];
+  String _title = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchData();
+  }
+
+  void _fetchData() async {
+    DocumentSnapshot ds = await widget.docRef.get();
+    if (ds.data['ide'] == null) return;
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _timController.text = ds.data['nama'];
+      _ideController.text = ds.data['ide'];
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text(_title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: <Widget>[
+              TextField(
+                controller: _timController,
+                decoration: InputDecoration(hintText: 'Nama Tim'),
+              ),
+              SizedBox(height: 16),
+              ToggleButtons(
+                children: <Widget>[
+                  Text('Mobile'),
+                  Text('Web'),
+                  Text('Game'),
+                  Text('Others'),
+                ],
+                isSelected: selections,
+                onPressed: (int index) {
+                  setState(() {
+                    selections[index] = !selections[index];
+                  });
+                },
+              ),
+              SizedBox(height: 16),
+              TextField(
+                controller: _ideController,
+                keyboardType: TextInputType.multiline,
+                minLines: 3,
+                maxLines: 20,
+                decoration: InputDecoration(hintText: 'Ide produk'),
+              ),
+              SizedBox(height: 16),
+              RaisedButton(
+                color: Colors.blue,
+                textColor: Colors.white,
+                child: Text('GAS'),
+                onPressed: ()async {
+                  if (_timController.text.isEmpty ||
+                      _ideController.text.isEmpty) return;
+                  await widget.docRef.updateData({
+                    'nama':_timController.text,
+                    'ide':_ideController.text,
+                    'ubah_id': FieldValue.increment(1),
+                    'waktu_ide': FieldValue.serverTimestamp(),
+                  });  
+                  setState(() {
+                   _title = ':)'; 
+                  });
+                },
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
